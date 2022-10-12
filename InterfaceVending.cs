d846c6b21fee8bd5750c97afa23f29e649374755
DevSeif <65452750+DevSeif@users.noreply.github.com>
2022-10-12 14:45:11 +0200

    Add files via upload

---
+++ InterfaceVending.cs
@@ -0,0 +1,13 @@
+﻿namespace Vending_Machine
+{
+    public interface IVending
+    {
+        Product Purchase(int argIndex);
+
+        void ShowAll();
+
+        void InsertMoney(int money);
+
+        Dictionary<int, int> EndTransaction();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Vending_Machine;
+
+namespace MyApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("\n\n1. Använd vending machine \n2. Kolla vad du har köpt \n: ");
+                try
+                {
+                    int val = Convert.ToInt32(Console.ReadLine());
+                
+                switch (val)
+                {
+                    case 1:
+                        VendingMachine.InputMoney();
+                        break;
+                    case 2:
+                        VendingMachine.CheckBoughtProducts();
+                        break;
+                }
+                }
+                catch
+                {
+                    Console.WriteLine("Ogiltig input");
+                }
+            }
+
+        }
+
+        
+    }
+}
