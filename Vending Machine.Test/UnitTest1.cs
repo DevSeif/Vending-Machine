1fb4e76228c4d85678c760507e2d711f917185d3
DevSeif <65452750+DevSeif@users.noreply.github.com>
2022-10-13 11:44:07 +0200

    Update UnitTest1.cs

--- Vending Machine.Test/UnitTest1.cs
+++ Vending Machine.Test/UnitTest1.cs
@@ -9,38 +9,23 @@ namespace Vending_Machine.Test
         public void CorrectInput_Test()
         {
             Assert.True(machine.CheckInput(20));
-            Assert.False(machine.CheckInput(21));
+            Assert.False(machine.CheckInput(21));  
         }
 
         [Fact]
         public void EnoughMoney_Test()
         {
-            int poolMoney = 50;
-            List<Product> products = new List<Product>
-        {
-            new Snack(12, "Snickers"),
-
-        };
-            Assert.True(poolMoney >= products[0].Price);
+            VendingMachine.poolMoney = 50;
+            List<Product> myProduct = VendingMachine.products;
+            Assert.True(VendingMachine.poolMoney >= myProduct[0].Price);
         }
 
         [Fact]
         public void EndTransaction_Test()
         {
             int i = 1;
-            int poolMoney = 127;
-            int[] moneyDenominations = { 1, 5, 10, 20, 50, 100, 500, 1000 };
-            Dictionary<int, int> myDictionary = new Dictionary<int, int>();
-
-            foreach (int e in moneyDenominations.Reverse())
-            {
-                while (e <= poolMoney)
-                {
-                    myDictionary.Add(i, e);
-                    i++;
-                    poolMoney -= e;
-                }
-            }
+            VendingMachine.poolMoney = 127;
+            Dictionary<int, int> myDictionary = machine.EndTransaction();
 
             Assert.Equal(100, myDictionary[1]);
             Assert.Equal(20, myDictionary[2]);
@@ -52,25 +37,16 @@ namespace Vending_Machine.Test
         [Fact]
         public void Purchase_Test()
         {
-            List<Product> products = new List<Product>();
-            products.Add(new Snack(12, "Snickers"));
-            int argIndex = 0;
-            int poolMoney = 15;
-            int answer = poolMoney - products[argIndex].Price;
 
-            Product product = products[argIndex];
-            poolMoney -= products[argIndex].Price;
-            products.RemoveAt(argIndex);
+            VendingMachine.poolMoney = 650;
+            int lastIndex = VendingMachine.products.Count() - 1;
 
-            Assert.Empty(products);
-            Assert.Equal(answer, poolMoney);
+            int answer = VendingMachine.poolMoney - VendingMachine.products[lastIndex].Price;
+            Product myProduct = machine.Purchase(lastIndex);
 
+            Assert.Equal("Kubansk cigarette", myProduct.Name);
+            Assert.Equal(answer, VendingMachine.poolMoney);
         }
-
-
-
-
-
 
     }
 }
